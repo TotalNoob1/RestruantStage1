f3d3942fffbf6f13e91ff38ba6374325a3321013
Edward V Cruz <edwardcruzcoding@gmail.com>
2018-10-15 17:57:03 -0500

    Feat: Made the initial view responsive for mobile devices

--- js/main.js
+++ js/main.js
@@ -158,6 +158,8 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
+  li.classList.add('infoRest')
+
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
@@ -178,7 +180,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  li.append(more);
 
   return li
 }
